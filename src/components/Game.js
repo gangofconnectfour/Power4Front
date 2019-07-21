24baeb345cd1038002288b92dad7e77a65dbf140
Barogthor <f.thery3103@gmail.com>
2019-07-21 23:43:00 +0200

    save me condition (╯°□°）╯︵ ┻━┻)

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 import {connect} from "react-redux";
 import {sign_out} from "../actions/userActions";
 import GameCell from "./Cell"
+import SockJsClient from 'react-stomp';
+
 
 
 class Game extends Component{
@@ -10,10 +12,68 @@ class Game extends Component{
         super(props)
         this.hoverColumn = this.hoverColumn.bind(this)
         this.clickColumn = this.clickColumn.bind(this)
+        this.onMessageReceive = this.onMessageReceive.bind(this)
+        this.sendMessage = this.sendMessage.bind(this)
+        this.conditionVictory = this.conditionVictory.bind(this)
 
         const board = Array(42)
         for(var i=0;i<board.length;i++) board[i]={pawn:0,highlighted:false}
         this.state = {board:[...board],highlightedCell:false,yourTurn:true}
+    }
+
+
+    leftHoz(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column < 0) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        return this.leftHoz(board, row, column-1, pawnOwner, repeat+1)
+    }
+
+    rightHoz(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column > 6) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        return this.rightHoz(board, row, column+1, pawnOwner, repeat+1)
+    }
+
+    diagonalLeftTop(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column > 6 || row < 0) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        return this.diagonalLeftTop(board, row-1, column+1, pawnOwner, repeat+1)
+    }
+
+    diagonalLeftBot(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column < 0 || row > 5) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        return this.diagonalLeftBot(board, row+1, column-1, pawnOwner, repeat+1)
+    }
+
+    diagonalRightTop(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column < 0 || row > 5) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        return this.diagonalRightTop(board, row+1, column-1, pawnOwner, repeat+1)
+    }
+
+    diagonalRightBot(board, row, column, pawnOwner, repeat){
+        if(repeat > 6) return repeat
+        if(column > 6 || row < 0) return repeat
+        if(board[row*column].pawn !== pawnOwner || board[row*column].pawn === 0) return repeat
+        console.log("diagrightbot",repeat);
+        return this.diagonalRightBot(board, row-1, column+1, pawnOwner, repeat+1)
+    }
+
+
+    conditionVictory(board, row, column, pawnOwner){
+        let currentStreak = 1
+        console.log(board);
+        const hoz = this.leftHoz(board, row, column, pawnOwner, 0) + this.rightHoz(board, row, column, pawnOwner, 0)
+        const diagLeft = this.diagonalLeftTop(board, row, column, pawnOwner, 0) + this.diagonalLeftBot(board, row, column, pawnOwner, 0)
+        const diagRight = this.diagonalRightTop(board, row, column, pawnOwner, 0) + this.diagonalRightBot(board, row, column, pawnOwner, 0)
+
+        console.log(hoz, diagLeft, diagRight);
     }
 
     hoverColumn(column){
@@ -33,10 +93,12 @@ class Game extends Component{
         if(this.state.highlightedCell)
             newBoard[this.state.highlightedCell].highlighted = false
 
+
         this.setState({board:newBoard,highlightedColumn:column,highlightedCell:targetCell})
     }
 
     clickColumn(column){
+        if(!this.state.yourTurn) return
         const walkColumn = this.state.board.filter((cell,index) => index%7===column).reverse()
         const targetRow = walkColumn.findIndex(cell => {
             return cell.pawn === 0
@@ -45,11 +107,45 @@ class Game extends Component{
 
         const targetCell = 35-targetRow*7+column
         const newBoard = [...this.state.board]
-        newBoard[targetCell].pawn = this.state.yourTurn ? 1 : 2
+        newBoard[targetCell].pawn = 1
         if(this.state.highlightedCell)
             newBoard[this.state.highlightedCell].highlighted = false
 
-        this.setState({board:newBoard,highlightedColumn:false,highlightedCell:false,yourTurn:!this.state.yourTurn})
+
+        this.sendMessage(newBoard.map(n => n.pawn))
+        this.conditionVictory(newBoard, targetRow, column, 1)
+        this.setState({board:newBoard,highlightedColumn:false,highlightedCell:false,yourTurn:false})
+    }
+
+    onMessageReceive(response, topic) {
+        const column = JSON.parse(response.body).hit
+
+        const walkColumn = this.state.board.filter((cell,index) => index%7===column).reverse()
+        const targetRow = walkColumn.findIndex(cell => {
+            return cell.pawn === 0
+        }) % 7
+        if(targetRow === -1) return
+
+        const targetCell = 35-targetRow*7+column
+        const newBoard = [...this.state.board]
+        newBoard[targetCell].pawn = 2
+        if(this.state.highlightedCell)
+            newBoard[this.state.highlightedCell].highlighted = false
+
+        this.conditionVictory(newBoard, targetRow, column, 2)
+        this.setState({board:newBoard,highlightedColumn:false,highlightedCell:false,yourTurn:true})
+
+    }
+
+    sendMessage (board) {
+        try {
+            this.clientRef.sendMessage("/app/hello", JSON.stringify(board));
+            console.log("oui");
+            return true;
+        } catch(e) {
+            console.log("fuck");
+            return false;
+        }
     }
 
     render(){
@@ -65,6 +161,11 @@ class Game extends Component{
                     )
                 }
                 </div>
+                <SockJsClient url='http://localhost:8090/rtc' topics={['/topic/greetings']}
+                              onMessage={ this.onMessageReceive } ref={ (client) => { this.clientRef = client }}
+                              onConnect={ () => { console.log("connected"); this.setState({ clientConnected: true }) } }
+                              onDisconnect={ () => { console.log("disconnected"); this.setState({ clientConnected: false }) } }
+                              debug={ false }/>
             </Fragment>
         )
     }
